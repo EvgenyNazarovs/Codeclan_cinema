927fdbce948d7cfb0d606fa9e045ba3f2f2cd484
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-03-28 09:37:45 +0000

    adds buy ticket functionality

--- models/ticket.rb
+++ models/ticket.rb
@@ -2,6 +2,9 @@ require_relative('../db/sql_runner')
 
 class Ticket
 
+  attr_accessor :customer_id, :film_id
+  attr_reader :id
+
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @customer_id = options['customer_id']
@@ -14,6 +17,40 @@ class Ticket
            RETURNING *"
     values = [@customer_id, @film_id]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+    buy_ticket()
+  end
+
+  def film
+    sql = "SELECT * FROM films
+           WHERE id = $1"
+    values = [@film_id]
+    result = SqlRunner.run(sql, values)[0]
+    return Film.new(result)
+  end
+
+  def price
+    result = film()
+    return film.price
+  end
+
+  def customer_funds
+    result = customer()
+    return customer.funds
+  end
+
+  def buy_ticket
+    customer = customer()
+    price = price().to_i
+    customer.funds = customer.funds.to_i - price
+    customer.update
+  end
+
+  def customer
+    sql = "SELECT * FROM customers
+           WHERE id = $1"
+    values = [@customer_id]
+    customer = SqlRunner.run(sql, values).first
+    return Customer.new(customer)
   end
 
   def self.delete_all
